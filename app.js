c26755879b49b388248f6e70fbebc6d528fd273d
Haiyang Hong <hong961127@gmail.com>
2020-07-23 01:50:19 -0700

    Setup react app

--- app.js
+++ app.js
@@ -31,49 +31,11 @@ const parseCSV = async () => {
           // Filter out those children, only the 'CEO' is left
           .includes(employee.id)
     )[0];
-
-    console.log(JSON.stringify(hierarchicalData));
   } catch (error) {
     console.error(error);
   }
 };
 parseCSV();
-
-// console.log(
-//   JSON.stringify({
-//     id: "n1",
-//     name: "Lao Lao",
-//     title: "general manager",
-//     children: [
-//       { id: "n2", name: "Bo Miao", title: "department manager" },
-//       {
-//         id: "n3",
-//         name: "Su Miao",
-//         title: "department manager",
-//         children: [
-//           { id: "n4", name: "Tie Hua", title: "senior engineer" },
-//           {
-//             id: "n5",
-//             name: "Hei Hei",
-//             title: "senior engineer",
-//             children: [
-//               { id: "n6", name: "Dan Dan", title: "engineer" },
-//               { id: "n7", name: "Xiang Xiang", title: "engineer" },
-//             ],
-//           },
-//           { id: "n8", name: "Pang Pang", title: "senior engineer" },
-//         ],
-//       },
-//       { id: "n9", name: "Hong Miao", title: "department manager" },
-//       {
-//         id: "n10",
-//         name: "Chun Miao",
-//         title: "department manager",
-//         children: [{ id: "n11", name: "Yue Yue", title: "senior engineer" }],
-//       },
-//     ],
-//   })
-// );
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server up on port ${port}...`));
