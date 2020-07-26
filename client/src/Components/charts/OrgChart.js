02bb07bff47f2234582d4fe45b9f798297267525
Haiyang Hong <hong961127@gmail.com>
2020-07-25 20:28:23 -0700

    Connect chart

--- client/src/Components/charts/OrgChart.js
+++ client/src/Components/charts/OrgChart.js
@@ -1,91 +1,18 @@
 import React, { Fragment, useRef } from "react";
 import OrganizationChart from "@dabeng/react-orgchart";
 import OrgChartNode from "./OrgChartNode";
+import { connect } from "react-redux";
 
 import ZoomControl from "./ZoomControl";
 import PanControl from "./PanControl";
+import exampleData from "../../utils/exampleData";
 import "./OrgChart.scss";
 
-const OrgChart = () => {
+const OrgChart = ({ orgData }) => {
   const orgChartRef = useRef(null);
 
   const handleDownload = async () => {
     orgChartRef.current.exportTo("chart", "png");
-  };
-
-  const ds = {
-    id: "n1",
-    name: "Lao Lao",
-    title: "general manager",
-    email: "example@gmail.com",
-    children: [
-      {
-        id: "n2",
-        name: "Bo Miao",
-        title: "department manager",
-        email: "example@gmail.com",
-      },
-      {
-        id: "n3",
-        name: "Su Miao",
-        title: "department manager",
-        email: "example@gmail.com",
-        children: [
-          {
-            id: "n4",
-            name: "Tie Hua",
-            title: "senior engineer",
-            email: "example@gmail.com",
-          },
-          {
-            id: "n5",
-            name: "Hei Hei",
-            title: "senior engineer",
-            email: "example@gmail.com",
-            children: [
-              {
-                id: "n6",
-                name: "Dan Dan",
-                title: "engineer",
-                email: "example@gmail.com",
-              },
-              {
-                id: "n7",
-                name: "Xiang Xiang",
-                title: "engineer",
-                email: "example@gmail.com",
-              },
-            ],
-          },
-          {
-            id: "n8",
-            name: "Pang Pang",
-            title: "senior engineer",
-            email: "example@gmail.com",
-          },
-        ],
-      },
-      {
-        id: "n9",
-        name: "Hong Miao",
-        title: "department manager",
-        email: "example@gmail.com",
-      },
-      {
-        id: "n10",
-        name: "Chun Miao",
-        title: "department manager",
-        email: "example@gmail.com",
-        children: [
-          {
-            id: "n11",
-            name: "Yue Yue",
-            title: "senior engineer",
-            email: "example@gmail.com",
-          },
-        ],
-      },
-    ],
   };
 
   return (
@@ -94,7 +21,7 @@ const OrgChart = () => {
       <PanControl />
       <OrganizationChart
         ref={orgChartRef}
-        datasource={ds}
+        datasource={orgData || exampleData}
         chartClass="myChart"
         NodeTemplate={OrgChartNode}
         draggable={true}
@@ -104,4 +31,6 @@ const OrgChart = () => {
   );
 };
 
-export default OrgChart;
+const mapStateToProps = ({ orgData }) => ({ orgData });
+
+export default connect(mapStateToProps)(OrgChart);
---
+++ client/src/utils/exampleData.js
@@ -0,0 +1,74 @@
+export default {
+  id: "n1",
+  name: "Lao Lao",
+  title: "general manager",
+  email: "example@gmail.com",
+  children: [
+    {
+      id: "n2",
+      name: "Bo Miao",
+      title: "department manager",
+      email: "example@gmail.com",
+    },
+    {
+      id: "n3",
+      name: "Su Miao",
+      title: "department manager",
+      email: "example@gmail.com",
+      children: [
+        {
+          id: "n4",
+          name: "Tie Hua",
+          title: "senior engineer",
+          email: "example@gmail.com",
+        },
+        {
+          id: "n5",
+          name: "Hei Hei",
+          title: "senior engineer",
+          email: "example@gmail.com",
+          children: [
+            {
+              id: "n6",
+              name: "Dan Dan",
+              title: "engineer",
+              email: "example@gmail.com",
+            },
+            {
+              id: "n7",
+              name: "Xiang Xiang",
+              title: "engineer",
+              email: "example@gmail.com",
+            },
+          ],
+        },
+        {
+          id: "n8",
+          name: "Pang Pang",
+          title: "senior engineer",
+          email: "example@gmail.com",
+        },
+      ],
+    },
+    {
+      id: "n9",
+      name: "Hong Miao",
+      title: "department manager",
+      email: "example@gmail.com",
+    },
+    {
+      id: "n10",
+      name: "Chun Miao",
+      title: "department manager",
+      email: "example@gmail.com",
+      children: [
+        {
+          id: "n11",
+          name: "Yue Yue",
+          title: "senior engineer",
+          email: "example@gmail.com",
+        },
+      ],
+    },
+  ],
+};
