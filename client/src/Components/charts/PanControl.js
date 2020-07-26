df4854d0f0fa70c7124bd494a5b7967b8aee7726
Haiyang Hong <hong961127@gmail.com>
2020-07-25 19:19:57 -0700

    Finish pan control ui

--- client/src/Components/charts/PanControl.js
+++ client/src/Components/charts/PanControl.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./PanControl.scss";
+
 const PanControl = ({ setTranslateX, setTranslateY }) => {
   const handlePanLeft = () => {
     setTranslateX(
@@ -24,12 +26,24 @@ const PanControl = ({ setTranslateX, setTranslateY }) => {
       (prev) => `translateY(${parseFloat(prev.match(/[\-0-9]+/)[0]) + 100}px)`
     );
   };
+
   return (
     <div className="pan">
-      <button onClick={handlePanLeft}>Left</button>
-      <button onClick={handlePanRight}>Right</button>
-      <button onClick={handlePanUp}>Up</button>
-      <button onClick={handlePanDown}>Down</button>
+      <button className="pan-up" onClick={handlePanUp}>
+        <i class="fas fa-arrow-up"></i>
+      </button>
+
+      <div>
+        <button className="pan-left" onClick={handlePanLeft}>
+          <i class="fas fa-arrow-left"></i>
+        </button>
+        <button className="pan-right" onClick={handlePanRight}>
+          <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+      <button className="pan-bottom" onClick={handlePanDown}>
+        <i class="fas fa-arrow-down"></i>
+      </button>
     </div>
   );
 };
