23da3d4dd0808081ffd2f860ef21571c3c1177e4
Haiyang Hong <hong961127@gmail.com>
2020-07-26 22:57:22 -0700

    Factor out Showcase, Features component

--- client/src/Components/pages/Landing.js
+++ client/src/Components/pages/Landing.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 import Header from "../landing/Header";
+import Showcase from "../landing/Showcase";
+import Features from "../landing/Features";
 import ChartDemo from "../../assets/orgchart-demo.png";
 import Navbar from "../layout/Navbar";
 import "./Landing.scss";
@@ -11,104 +13,9 @@ const Landing = () => {
     <section className="landing">
       <Navbar />
       <Header />
+      <Showcase />
+      <Features />
 
-      <section className="showcase text-center">
-        <Container>
-          <h2>See Org Chart in action</h2>
-          <img src={ChartDemo} alt="Chart Demo" />
-          <p className="greyed-out mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-            accusamus, fuga officiis in at a quo voluptas modi optio facere
-            iusto distinctio dolorem, beatae alias?
-          </p>
-        </Container>
-      </section>
-      <section className="features text-center">
-        <Container>
-          <h2>Features</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-            hic.
-          </p>
-          <Row>
-            <Col xs={{ span: 6 }}>
-              <div className="feature-card">
-                <h3 className="feature-title">CSV to Org Chart</h3>
-                <p className="feature-text">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint
-                  similique repellat molestias fuga fugit esse, vitae ad
-                  nesciunt.
-                </p>
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="mt-3 text-muted border py-5">
-                NEED AN ILLUSTRATION HERE
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="mt-3 text-muted border py-5">
-                NEED AN ILLUSTRATION HERE
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="feature-card">
-                <h3 className="feature-title">Re-arrange Hierarchy</h3>
-                <p className="feature-text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Rerum quidem qui aliquam laudantium dolore quaerat quibusdam
-                  ullam eveniet soluta exercitationem!
-                </p>
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="feature-card">
-                <h3 className="feature-title">Add / Remove Employees</h3>
-                <p className="feature-text">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Iusto ipsa quis quia qui? Quae adipisci soluta sapiente
-                  voluptatum, quasi inventore.
-                </p>
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="mt-3 text-muted border py-5">
-                NEED AN ILLUSTRATION HERE
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="mt-3 text-muted border py-5">
-                NEED AN ILLUSTRATION HERE
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="feature-card">
-                <h3 className="feature-title">One-click Export</h3>
-                <p className="feature-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Quisquam tenetur soluta totam doloremque, modi ipsa vitae
-                  nihil illum.
-                </p>
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="feature-card">
-                <h3 className="feature-title">Free to Use!</h3>
-                <p className="feature-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-                  alias dolorem consequuntur? Quaerat, corporis dolorum!
-                </p>
-                <a className="feature-btn">Try Now</a>
-              </div>
-            </Col>
-            <Col xs={{ span: 6 }}>
-              <div className="mt-3 text-muted border py-5">
-                NEED AN ILLUSTRATION HERE
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
       <footer>
         <div class="custom-shape-divider-top-1595828527">
           <svg
