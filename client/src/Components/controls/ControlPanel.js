f3b4246f4d8674b04ce0729d02d58d7abb1608de
Haiyang Hong <hong961127@gmail.com>
2020-07-29 09:48:47 -0700

    Add tooltip

--- client/src/Components/controls/ControlPanel.js
+++ client/src/Components/controls/ControlPanel.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useRef, useEffect } from "react";
 import { connect } from "react-redux";
+import ReactTooltip from "react-tooltip";
 
 import { getTemplate, resetTemplate } from "../../actions/csvTemplateActions";
 import { uploadOrgData } from "../../actions/orgChartActions";
@@ -23,33 +24,50 @@ const ControlPanel = ({
   openSideDrawer,
 }) => {
   const [step, setStep] = useState(1);
+  let tipRef = useRef();
+
+  useEffect(() => {
+    if (!sideDrawer) {
+      ReactTooltip.show(tipRef);
+    }
+  }, [sideDrawer]);
 
   return (
-    <div className={`control-panel ${!sideDrawer ? "side-drawer-close" : ""}`}>
-      {/* <div className={`control-panel ${!sideDrawer ? "" : "side-drawer-close"}`}> */}
-      {sideDrawer ? (
-        <div className="toggler-btn" onClick={closeSideDrawer}>
-          <i className="fas fa-chevron-left"></i>
-        </div>
-      ) : (
-        <div className="open-icon" onClick={openSideDrawer}>
-          <i class="fas fa-chevron-right"></i>
-        </div>
-      )}
-      {sideDrawer && step === 1 && (
-        <StepOne
-          template={template}
-          getTemplate={getTemplate}
-          resetTemplate={resetTemplate}
-          setStep={setStep}
-        />
-      )}
-      {sideDrawer && step === 2 && <StepTwo setStep={setStep} />}
-      {sideDrawer && step === 3 && (
-        <StepThree setStep={setStep} uploadOrgData={uploadOrgData} />
-      )}
-      {sideDrawer && step === 4 && <StepFour setStep={setStep} />}
-    </div>
+    <Fragment>
+      <div
+        className={`control-panel ${!sideDrawer ? "side-drawer-close" : ""}`}
+      >
+        {/* <div className={`control-panel ${!sideDrawer ? "" : "side-drawer-close"}`}> */}
+        {sideDrawer ? (
+          <div className="toggler-btn" onClick={closeSideDrawer}>
+            <i className="fas fa-chevron-left"></i>
+          </div>
+        ) : (
+          <div
+            className="open-icon"
+            onClick={openSideDrawer}
+            data-tip="Click to get started !"
+            ref={(ref) => (tipRef = ref)}
+          >
+            <i class="fas fa-chevron-right"></i>
+          </div>
+        )}
+        {sideDrawer && step === 1 && (
+          <StepOne
+            template={template}
+            getTemplate={getTemplate}
+            resetTemplate={resetTemplate}
+            setStep={setStep}
+          />
+        )}
+        {sideDrawer && step === 2 && <StepTwo setStep={setStep} />}
+        {sideDrawer && step === 3 && (
+          <StepThree setStep={setStep} uploadOrgData={uploadOrgData} />
+        )}
+        {sideDrawer && step === 4 && <StepFour setStep={setStep} />}
+      </div>
+      <ReactTooltip />
+    </Fragment>
   );
 };
 
