ef6becc4369709976755061700b38814ff76e03b
Haiyang Hong <hong961127@gmail.com>
2020-07-30 00:09:48 -0700

    Align button height from step 1 to 5

--- client/src/Components/controls/StepOne.js
+++ client/src/Components/controls/StepOne.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import csvEmpty from "../../assets/csv-empty.png";
 import "./StepOne.scss";
@@ -10,41 +10,49 @@ const StepOne = ({ template, getTemplate, resetTemplate, setStep }) => {
   };
 
   return (
-    <div className="step-one">
-      <div className="step-one-progress-text">
-        <span className="st-1 text-center">
-          1. Download <br /> <span>Template</span>
-        </span>
-        <span className="st-2 ">2. Edit & Save</span>
-        <span className="st-3">3. Upload File</span>
-        <span className="st-4">4. Generate Chart</span>
-      </div>
-      <div className="step-one-progress">
-        <div className="line"></div>
-        <div className="milestone active ms-1"></div>
-        <div className="milestone ms-2"></div>
-        <div className="milestone ms-3"></div>
-        <div className="milestone ms-4"></div>
-      </div>
-      <h2 className="step-one-heading">STEP 1</h2>
-      <p className="step-one-msg">
-        Download our <span>.CSV</span> template
-      </p>
-      <img src={csvEmpty} className="step-one-csv" alt="empty-csv-screenshot" />
-      <button className="step-one-download" onClick={getTemplate}>
-        Download file <i className="fas fa-download"></i>
-      </button>
-      <hr />
-      {template ? (
-        <button className="step-one-continue" onClick={handleNextStep}>
-          Continue
+    <Fragment>
+      <div className="step-one">
+        <div className="step-one-progress-text">
+          <span className="st-1 text-center">
+            1. Download <br /> <span>Template</span>
+          </span>
+          <span className="st-2 ">2. Edit & Save</span>
+          <span className="st-3">3. Upload File</span>
+          <span className="st-4">4. Generate Chart</span>
+        </div>
+        <div className="step-one-progress">
+          <div className="line"></div>
+          <div className="milestone active ms-1"></div>
+          <div className="milestone ms-2"></div>
+          <div className="milestone ms-3"></div>
+          <div className="milestone ms-4"></div>
+        </div>
+        <h2 className="step-one-heading">STEP 1</h2>
+        <p className="step-one-msg">
+          Download our <span>.CSV</span> template
+        </p>
+        <img
+          src={csvEmpty}
+          className="step-one-csv"
+          alt="empty-csv-screenshot"
+        />
+        <button className="step-one-download" onClick={getTemplate}>
+          Download file <i className="fas fa-download"></i>
         </button>
-      ) : (
-        <button className="step-one-continue" disabled>
-          Continue
-        </button>
-      )}
-    </div>
+      </div>
+      <div className="stepone-actions">
+        <hr />
+        {template ? (
+          <button className="step-one-continue" onClick={handleNextStep}>
+            Continue
+          </button>
+        ) : (
+          <button className="step-one-continue" disabled>
+            Continue
+          </button>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
