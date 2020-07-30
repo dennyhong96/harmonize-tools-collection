d7413664d13aa6daa679d547c79d36bea213346d
Haiyang Hong <hong961127@gmail.com>
2020-07-29 18:09:08 -0700

    sidedrawer expand by default, get rid of topbar toggle button

--- client/src/Components/controls/ControlPanel.js
+++ client/src/Components/controls/ControlPanel.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import ReactTooltip from "react-tooltip";
 
@@ -24,50 +24,33 @@ const ControlPanel = ({
   openSideDrawer,
 }) => {
   const [step, setStep] = useState(1);
-  let tipRef = useRef();
-
-  useEffect(() => {
-    if (!sideDrawer) {
-      ReactTooltip.show(tipRef);
-    }
-  }, [sideDrawer]);
 
   return (
-    <Fragment>
-      <div
-        className={`control-panel ${!sideDrawer ? "side-drawer-close" : ""}`}
-      >
-        {/* <div className={`control-panel ${!sideDrawer ? "" : "side-drawer-close"}`}> */}
-        {sideDrawer ? (
-          <div className="toggler-btn" onClick={closeSideDrawer}>
-            <i className="fas fa-chevron-left"></i>
-          </div>
-        ) : (
-          <div
-            className="open-icon"
-            onClick={openSideDrawer}
-            data-tip="Click to get started !"
-            ref={(ref) => (tipRef = ref)}
-          >
-            <i class="fas fa-chevron-right"></i>
-          </div>
-        )}
-        {sideDrawer && step === 1 && (
-          <StepOne
-            template={template}
-            getTemplate={getTemplate}
-            resetTemplate={resetTemplate}
-            setStep={setStep}
-          />
-        )}
-        {sideDrawer && step === 2 && <StepTwo setStep={setStep} />}
-        {sideDrawer && step === 3 && (
-          <StepThree setStep={setStep} uploadOrgData={uploadOrgData} />
-        )}
-        {sideDrawer && step === 4 && <StepFour setStep={setStep} />}
-      </div>
-      <ReactTooltip />
-    </Fragment>
+    <div className={`control-panel ${!sideDrawer ? "side-drawer-close" : ""}`}>
+      {/* <div className={`control-panel ${!sideDrawer ? "" : "side-drawer-close"}`}> */}
+      {sideDrawer ? (
+        <div className="toggler-btn" onClick={closeSideDrawer}>
+          <i className="fas fa-chevron-left"></i>
+        </div>
+      ) : (
+        <div className="open-icon" onClick={openSideDrawer}>
+          <i class="fas fa-chevron-right"></i>
+        </div>
+      )}
+      {sideDrawer && step === 1 && (
+        <StepOne
+          template={template}
+          getTemplate={getTemplate}
+          resetTemplate={resetTemplate}
+          setStep={setStep}
+        />
+      )}
+      {sideDrawer && step === 2 && <StepTwo setStep={setStep} />}
+      {sideDrawer && step === 3 && (
+        <StepThree setStep={setStep} uploadOrgData={uploadOrgData} />
+      )}
+      {sideDrawer && step === 4 && <StepFour setStep={setStep} />}
+    </div>
   );
 };
 
