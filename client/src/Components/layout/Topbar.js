1e6c19b303a61e2e2179b73a0ad622ca113272c5
Haiyang Hong <hong961127@gmail.com>
2020-08-02 07:01:57 -0700

    Add import button on topbar

--- client/src/Components/layout/Topbar.js
+++ client/src/Components/layout/Topbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 
@@ -6,11 +6,12 @@ import {
   openSideDrawer,
   closeSideDrawer,
 } from "../../actions/sideDrawerAction";
+import ConfirmNewChartPopup from "../charts/ConfirmNewChartPopup";
 import { logout, login } from "../../actions/userActions";
 import { startNewChart } from "../../actions/orgChartActions";
 import "./Topbar.scss";
 
-const topbar = ({
+const Topbar = ({
   sideDrawer,
   closeSideDrawer,
   openSideDrawer,
@@ -20,46 +21,58 @@ const topbar = ({
   history,
   login,
 }) => {
+  const [newChartPopupShow, setNewChartPopupShow] = useState(false);
+
   return (
-    <div className={`topbar ${sideDrawer ? "sidedrawer-show" : ""}`}>
-      {/* <div
+    <Fragment>
+      <div className={`topbar ${sideDrawer ? "sidedrawer-show" : ""}`}>
+        {/* <div
         className={`toggler-btn ${sideDrawer ? "opened" : ""}`}
         onClick={toggleSideDrawer}
       >
         <i className="fas fa-chevron-right"></i>
       </div> */}
-      <div className="">
-        <h1 className="heading">ORGANIZATIONAL CHART TOOL</h1>
-        <p className="sub-heading">
-          Begin creating your chart by{" "}
-          <span className="upload" onClick={() => openSideDrawer()}>
-            uploading your employee data
-          </span>{" "}
-          or by <span onClick={startNewChart}>starting from scratch</span>{" "}
-          below.
-        </p>
-      </div>
-
-      <div className="d-flex">
-        <div className="topbar-actions">
-          {user ? (
-            <button onClick={logout}>
-              <i class="fas fa-sign-out-alt"></i> logout
+        <div className="">
+          <h1 className="heading">ORGANIZATIONAL CHART TOOL</h1>
+          <p className="sub-heading">
+            Begin{" "}
+            <span onClick={() => setNewChartPopupShow(true)}>
+              editing your chart
+            </span>{" "}
+            below, or import your employee data
+            <button className="import-btn" onClick={() => openSideDrawer()}>
+              IMPORT
             </button>
-          ) : (
-            <button onClick={login}>
-              <i class="fab fa-google"></i> sign in with google
-            </button>
-          )}
-          <Link to="/">HOME</Link>
+          </p>
         </div>
-        {user && (
-          <div className="avatar">
-            <img src={user.photo} alt="user photo" />
+
+        <div className="d-flex">
+          <div className="topbar-actions">
+            {user ? (
+              <button onClick={logout}>
+                <i class="fas fa-sign-out-alt"></i> logout
+              </button>
+            ) : (
+              <button onClick={login}>
+                <i class="fab fa-google"></i> sign in with google
+              </button>
+            )}
+            <Link to="/">HOME</Link>
           </div>
-        )}
+          {user && (
+            <div className="avatar">
+              <img src={user.photo} alt="user photo" />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <ConfirmNewChartPopup
+        show={newChartPopupShow}
+        onHide={() => setNewChartPopupShow(false)}
+        setNewChartPopupShow={setNewChartPopupShow}
+        startNewChart={startNewChart}
+      />
+    </Fragment>
   );
 };
 
@@ -71,7 +84,7 @@ export default connect(mapStateToProps, {
   logout,
   startNewChart,
   login,
-})(withRouter(topbar));
+})(withRouter(Topbar));
 
 {
   /* <a
