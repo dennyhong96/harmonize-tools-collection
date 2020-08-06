9b37cfeccc6f61e972353c194d3bcfde66fd217f
Haiyang Hong <hong961127@gmail.com>
2020-08-06 10:51:13 -0700

    Fix route not found problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
 import Landing from "./Components/pages/Landing";
 import Home from "./Components/pages/Home";
+import CatchAll from "./Components/layout/CatchAll";
 import "./App.scss";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/app" component={Home} />
+        <Route component={CatchAll} />
       </Switch>
       <ToastContainer />
     </BrowserRouter>
