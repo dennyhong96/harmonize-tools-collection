7e36e65b73b0540bc92c3fc6443b6c5ec3d099dc
Haiyang Hong <hong961127@gmail.com>
2020-09-09 23:39:43 -0700

    Update contract

---
+++ clients/contract/build/precache-manifest.520d43add7f441737b672e00666a1fd9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23644a0f923a6c24e32f377e174ef0c9",
+    "url": "/contract/index.html"
+  },
+  {
+    "revision": "bbb3c2119f5ed10e2dd7",
+    "url": "/contract/static/css/2.6a68c842.chunk.css"
+  },
+  {
+    "revision": "5d7941bdfba0e54433e8",
+    "url": "/contract/static/css/main.3de35e61.chunk.css"
+  },
+  {
+    "revision": "bbb3c2119f5ed10e2dd7",
+    "url": "/contract/static/js/2.25f03a3b.chunk.js"
+  },
+  {
+    "revision": "a87075651ccd8d279f01c7e1b2e30179",
+    "url": "/contract/static/js/2.25f03a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7941bdfba0e54433e8",
+    "url": "/contract/static/js/main.321be326.chunk.js"
+  },
+  {
+    "revision": "d9de8f0c3e6ac54224ed",
+    "url": "/contract/static/js/runtime-main.04aab100.js"
+  },
+  {
+    "revision": "4e8feb17ad410b662baabc1abb3fdc03",
+    "url": "/contract/static/media/MainHeader.4e8feb17.png"
+  },
+  {
+    "revision": "17d4f5834ab58968da03d308c35f27cc",
+    "url": "/contract/static/media/MainIntro.17d4f583.png"
+  },
+  {
+    "revision": "029cb578f97b64d0969460717028980a",
+    "url": "/contract/static/media/facebook.029cb578.svg"
+  },
+  {
+    "revision": "164a9672f46472ed956dbe89ed5860f4",
+    "url": "/contract/static/media/feature1.164a9672.png"
+  },
+  {
+    "revision": "1dbbef451a3efbd52a86bcc081fd4222",
+    "url": "/contract/static/media/form.1dbbef45.png"
+  },
+  {
+    "revision": "755d67c30e0585f03ca7b7badc7a5fca",
+    "url": "/contract/static/media/google.755d67c3.svg"
+  },
+  {
+    "revision": "0cdadc73e5e5ccd2c8dfa2ccb46efedf",
+    "url": "/contract/static/media/linkedin.0cdadc73.svg"
+  },
+  {
+    "revision": "5315eedec980e4348c45d6dca5acc08e",
+    "url": "/contract/static/media/twitter.5315eede.svg"
+  }
+]);
--- clients/contract/build/service-worker.js
+++ clients/contract/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contract/precache-manifest.888bb9b0a429f502b8d55b6944d774e0.js"
+  "/contract/precache-manifest.520d43add7f441737b672e00666a1fd9.js"
 );
 
 self.addEventListener('message', (event) => {
