5ab367d0f2d877eec1e3fa6b65da6ab1a84cb7a3
Haiyang Hong <hong961127@gmail.com>
2020-09-10 00:30:29 -0700

    Fix git ignore

---
+++ clients/mainpage/build/precache-manifest.2caa6caa1125163476d91fbdc2538c1f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355ef9bc49a58d6496b4bd55310d243d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cb0bb5694d435d5b76",
+    "url": "/static/css/main.4938e564.chunk.css"
+  },
+  {
+    "revision": "7331ca4cae5a198b5d2a",
+    "url": "/static/js/2.a9b0f3b9.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.a9b0f3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cb0bb5694d435d5b76",
+    "url": "/static/js/main.e66c8deb.chunk.js"
+  },
+  {
+    "revision": "d33770a8195ba6c1c56b",
+    "url": "/static/js/runtime-main.c4a7ca78.js"
+  },
+  {
+    "revision": "029cb578f97b64d0969460717028980a",
+    "url": "/static/media/facebook.029cb578.svg"
+  },
+  {
+    "revision": "755d67c30e0585f03ca7b7badc7a5fca",
+    "url": "/static/media/google.755d67c3.svg"
+  },
+  {
+    "revision": "0cdadc73e5e5ccd2c8dfa2ccb46efedf",
+    "url": "/static/media/linkedin.0cdadc73.svg"
+  },
+  {
+    "revision": "ea5fcf4afc83b7c336effd52365454b0",
+    "url": "/static/media/medium.ea5fcf4a.svg"
+  },
+  {
+    "revision": "5315eedec980e4348c45d6dca5acc08e",
+    "url": "/static/media/twitter.5315eede.svg"
+  }
+]);
